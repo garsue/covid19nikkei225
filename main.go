7a99a16c73ccbe2ba48fe82f6346fcc6e8bd5f7b
garsue <osuga.shunya@mfkessai.co.jp>
2020-09-26 19:52:09 +0900

    Load Nikkei225

--- main.go
+++ main.go
@@ -16,11 +16,18 @@ type Daily struct {
 	Count int
 }
 
+// NI225 日経225インデックス
+type NI225 struct {
+	Date  string
+	Price float64
+}
+
 // Record JOIN後のレコード
 type Record struct {
 	Date     string
 	Tested   int
 	Positive int
+	NI225    float64
 }
 
 func main() {
@@ -45,6 +52,12 @@ func index(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
+	ni225, err := loadNI225()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 
 	var records []Record
 	for _, t := range tested {
@@ -57,6 +70,12 @@ func index(w http.ResponseWriter, r *http.Request) {
 		for _, p := range positive {
 			if p.Date == record.Date {
 				record.Positive = p.Count
+			}
+		}
+		// Look up the Nikkei225 price
+		for _, n := range ni225 {
+			if n.Date == record.Date {
+				record.NI225 = n.Price
 			}
 		}
 
@@ -98,6 +117,36 @@ func loadDailyCountCSV(filename string) ([]Daily, error) {
 		ds = append(ds, Daily{
 			Date:  record[0],
 			Count: count,
+		})
+	}
+}
+
+func loadNI225() ([]NI225, error) {
+	file, err := os.Open("ni225.csv")
+	if err != nil {
+		return nil, err
+	}
+	reader := csv.NewReader(file)
+	// Drop a header record
+	if _, err := reader.Read(); err != nil {
+		return nil, err
+	}
+	var ns []NI225
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			return ns, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		price, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			return nil, err
+		}
+		ns = append(ns, NI225{
+			Date:  record[0],
+			Price: price,
 		})
 	}
 }
